77f3b0bcfd7dc5d0856f66d927037b642d589693
Cameron Hardy <camhardy99@gmail.com>
2021-08-30 14:35:10 -0500

    exercises 1 and 2 complete

--- app.js
+++ app.js
@@ -3,7 +3,28 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+
+function arrSum(numbers) {
+    let sum = 0;
+
+    numbers.forEach((number) => (sum += number));
+    return sum;
+}
+
+console.log(arrSum(numbers));
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+let book = {};
 
+book.title = "Harry Potter and the Sorcerer's Stone";
+book.author = "J. K. Rowling";
+book.pages = "223";
+book.readCount = 0;
+book.info = function () {
+    return `Title: ${this.title} - Author: ${this.author} - Pages: ${this.pages}`;
+};
+
+console.log(book.info());
